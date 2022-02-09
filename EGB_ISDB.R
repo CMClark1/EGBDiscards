ea665e3af1329452dc396da41463920f4ffd5ed6
Clark <Caira.Clark@dfo-mpo.gc.ca>
2022-02-09 13:39:02 -0400

    fix table connections

--- EGB_ISDB.R
+++ EGB_ISDB.R
@@ -1,7 +1,7 @@
 ####################
-# EGB Discard Data Pull - MARFIS
+# EGB Discard Data Pull - ISDB
 # Caira Clark
-# 26 January 2021
+# 9 Feb 2022
 ####################
 
 library(ROracle)
@@ -14,9 +14,13 @@ select a.CFV, a.VESSEL_NAME, b.TRIP, b.TRIPCD_ID, b.MARFIS_CONF_NUMBER, b.LANDIN
 
 from isdb.isvessels a, isdb.istrips b, isdb.issetprofile c, isdb.isgears d, isdb.isfishsets e
 
-where TO_CHAR(c.SETDATE,'yyyy')=2016 
+where TO_CHAR(c.SETDATE,'yyyy')=2021 
   and e.NAFAREA_ID like '5Z%'
-  
+  and b.VESS_ID = a.VESS_ID
+  and c.FISHSET_ID = e.FISHSET_ID
+  and b.TRIP_ID = e.TRIP_ID
+  and e.GEAR_ID = d.GEAR_ID
+
 group by a.CFV, a.VESSEL_NAME, b.TRIP, b.TRIPCD_ID, b.MARFIS_CONF_NUMBER, b.LANDING_DATE, d.GEARCD_ID, e.SETCD_ID
 
 having d.GEARCD_ID not in (71, 62)
