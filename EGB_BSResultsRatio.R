583bf91c445d37499f218a8508ac195d28375af3
Clark <Caira.Clark@dfo-mpo.gc.ca>
2022-02-24 15:28:15 +0000

    discard significance

--- EGB_BSResultsRatio.R
+++ EGB_BSResultsRatio.R
@@ -4,6 +4,8 @@
 # 18 February 2021
 ####################
 
+#Load ratio data file from APL runs
+
 data <- read.csv('S:/Science/Population Ecology/Georges Bank/Spec Comp/2021 2022/ratiotest.csv')
 
 #Remove multipliers that are less than zero
@@ -22,3 +24,9 @@ data <- data %>%  mutate(
 data$DISCARDS <- ifelse(data$DISCARDS>0,data$DISCARDS,0)
 
 data$DISCARDS[is.na(data$DISCARDS)] <- 0
+
+#Determine if the values are significant using BS 1000 APL file. Then create another data column for significant/not significant.
+
+data$SIGNIFICANT <- c(0,0,0,0,0,0,1,0,1,0,0)
+
+data$SIGDISCARDS <- data$DISCARDS*data$SIGNIFICANT
