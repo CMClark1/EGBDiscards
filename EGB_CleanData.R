1e91c065a0752b7fc243adc1d607bffe51af98df
Clark <Caira.Clark@dfo-mpo.gc.ca>
2022-03-02 16:33:07 +0000

    cod and pollock directed trips

--- EGB_CleanData.R
+++ EGB_CleanData.R
@@ -96,7 +96,7 @@ head(isdbtrips) #Load this in script EGB_ISDB.R
 
 isdb_noncom <- isdbtrips %>% filter(SETCD_ID == 7) %>% select(TRIP, SETCD_ID)
 
-marfis <- marfis %>% rename(TRIP = OBS_Trip)
+marfis <- marfis %>% rename(TRIP = TRIP.x)
 
 marfis <- left_join(marfis, isdb_noncom, by="TRIP")
 
@@ -129,15 +129,92 @@ sought2 <- read.csv('S:/Science/Population Ecology/Georges Bank/Spec Comp/2021 2
 
 sought <- rbind(sought1, sought2)
 
-sought_nocodhad <- sought %>% select(TRIP, SPECSCD_ID)
+noncodhad <- sought %>% group_by(TRIP) %>% filter(!SPECSCD_ID %in% c(10,11)) #non cod/had directed sets
 
-marfis <- left_join(marfis, sought_nocodhad, by="TRIP") #creates many more lines of data==
+
+
+marfis <- left_join(marfis, sought_nocodhad, by="TRIP") #creates many more lines of data
 
 noncodhad <- marfis %>% filter(!SPECSCD_ID %in% c(10, 11))
 
 
-#Check cod/had ratios
+#Check cod/had ratios and pollock ratios
+
+df <- marfis
+df$X100[is.na(df$X100)] <- 0 #replace NAs with zero
+df$X110[is.na(df$X110)] <- 0
+df$X170[is.na(df$X110)] <- 0
+
+#Potential to add that sets are eliminated when cod or haddock landings = 0
+
+#Remove trips where cod:had ratio for all sets in the trip are greater than 0.8
+
+df$codhad_ratio <- df$X100/df$X110 #Calculate cod:had ratio
+df$test1<-with(df, ifelse(codhad_ratio < 0.8, "met", "not met")) #Basic ifelse; evaluation of a value for each record/set.
+temp1<-subset(df, select=c('TRIP_ID','test1')) #Removes the value column, to help apply the test1 command to groups instead of individual sets.
+temp1<-unique(temp1) #removes replicates in the group-level test dataset.
+temp2 <- temp1 %>% group_by(TRIP_ID) %>% summarise(n = n()) %>% filter(n==1) #select trips where sets all met or all did not meet conditions
+temp3 <- temp1 %>% filter(TRIP_ID %in% temp2$TRIP_ID & test1 == "not met") #select trips where all sets did not meet the condition
+names(temp3)[2]<-'test2' #Renames the test field to faciliate merging with df
+df2<-merge(df, temp3, all=TRUE) #merges the two, identifying groups which did not meet the conditions for at least one set
+df2$test2<-with(df2, ifelse(is.na(test2), 'met',test2)) #Replaces 'NAs' in the test column (created NAs becaused join in only groups which DID NOT meet conditions at least once)
+df2$test1<-NULL
+rm(df, temp1, temp2, temp3)
+
+cod_directed1 <- df2 %>% filter(test2 == "not met")
+df <- df2 %>% filter(test2 == "met")
+
+#Remove sets at the end of the trip with a ratio greater than 0.8
+
+df$test2 <- NULL
+df$test1<-with(df, ifelse(codhad_ratio < 0.8, "met", "not met")) 
+temp1 <- df %>% filter(codhad_ratio > 0.8) %>% 
+  select(TRIP_ID) %>% distinct(TRIP_ID)  #select any remaining trips with a set with a codhad ratio greater than 0.8
+
+temp2 <- df %>% 
+  filter(TRIP_ID %in% temp1$TRIP_ID) %>% 
+  select(LOG_EFRT_STD_INFO_ID, TRIP_ID, codhad_ratio, test1) %>%
+  group_by(TRIP_ID) %>%
+  slice(which.max(LOG_EFRT_STD_INFO_ID)) %>%
+  filter(test1 == "not met") #select any trips where the last set has a codhad ratio greater than 0.8
+
+temp3 <- df %>%
+  filter(TRIP_ID %in% temp2$TRIP_ID) %>%
+  select(LOG_EFRT_STD_INFO_ID, TRIP_ID, codhad_ratio, test1) %>%
+  group_split(TRIP_ID) #trip dataframes in list. check to make sure 'not met' occurs only at the end of the trip
+
+temp4 <- df %>% 
+  filter(TRIP_ID %in% temp2$TRIP_ID) %>%
+  filter(test1 == "met") #select data from the trips with high codhad ratios that meet the condition
+
+temp5 <- df %>%
+  filter(!TRIP_ID %in% temp2$TRIP_ID) #select all data that are not from the trips with the high/late cod had ratios
+
+cod_directed2 <- df %>% filter(TRIP_ID %in% temp2$TRIP_ID) %>% filter(test1 == "not met")
+cod_directed <- rbind(cod_directed1, setNames(cod_directed2, names(cod_directed1))) 
+cod_directed$test2 <- NULL #dataframe of sets identified as cod directed
+
+df <- rbind(temp4, temp5)
+df$test1 <- NULL #dataframe of remaining sets identified as not cod directed
+
+rm(temp1, temp2, temp3, temp4, temp5)
+
+#Remove sets where pollock landings are greater than combined cod and haddock landings
+
+df$pol_ratio <- df$X170>(df$X100+df$X110)
+pol_directed <- df %>% filter(pol_ratio == "TRUE") #dataframe with pollock directed sets
+haddock_directed <- df %>% filter(pol_ratio == "FALSE") #dataframe with haddock directed sets
+
+
 #Remove unwanted records
+
+#quarters with 100% observer coverage
+#cod directed sets or trips
+#pollock directed sets or trips
+#no panel trips
+#non-commercial trips
+#trips with no zones
+
 #Aggregate data
 
 
